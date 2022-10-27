ee7a796feaaa107eabcb433c317e9e085a74b0c7
teodo <teodo@DESKTOP-LDJI32M>
2022-10-27 16:57:02 +0300

    Add project files.

---
+++ Data/Munteanu_Bianca_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Munteanu_Bianca_Lab2.Models;
+
+namespace Munteanu_Bianca_Lab2.Data
+{
+    public class Munteanu_Bianca_Lab2Context : DbContext
+    {
+        public Munteanu_Bianca_Lab2Context (DbContextOptions<Munteanu_Bianca_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Munteanu_Bianca_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Munteanu_Bianca_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Munteanu_Bianca_Lab2.Models.Author> Author { get; set; }
+    }
+}
